0b11e3df0f9f900b60680408d26a651b18b9cc2e
Wilson Neto <contato@wilsonneto.com.br>
2021-08-14 21:26:15 -0300

    uploading files, finished tests

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import cache from '../../cache';
 
 type Data = {
-  name: string
+  name: string,
+  visit: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  cache.counter = cache.counter + 1;
+  res.status(200).json({ name: 'John Doe', visit: cache.counter })
 }
